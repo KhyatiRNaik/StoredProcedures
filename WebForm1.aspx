7caf5a871b3123aa657f513fbc0fa23d1cefaa2c
Student <Student@BVIMIT-DSESK-01>
2024-04-10 16:41:33 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="StoredProcedures.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+   <center> <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="ShowEmpName" runat="server" Text="Show Employee Names" Font-Bold="True" OnClick="ShowEmpName_Click" /><br /><br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView><br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+       </center>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace StoredProcedures
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ShowEmpName_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Student\Documents\khyati.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand cmd = new SqlCommand("showName", conn);
+            SqlDataReader dr;
+
+            try
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                cmd.CommandText = "showName";
+                cmd.CommandType = CommandType.StoredProcedure;
+                dr = cmd.ExecuteReader();
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = "Error: " + ex.ToString();
+            }
+        }
+    }
+}
